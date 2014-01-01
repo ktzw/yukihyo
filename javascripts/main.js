b514dc7427ddce838df6ac56df312bd77dbf81df
ktzw <ktzw@users.noreply.github.com>
2014-01-02 08:22:01 +0900

    form

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,22 @@
-console.log('This would be the main JS file.');
+;(function(){
+
+    for (var i = 100; i >= -100; i--) {
+        $("#pay").append($("<option/>", {value: i, text: i}));
+    }
+
+    var getTip = function(val){
+        return $("#tip").data("value");
+    };
+    var setTip = function(val){
+        $("#tip").data("value", val);
+        $("#tip").text(val);
+    };
+
+    $("#add3tip").click(function() { setTip(getTip() + 3); });
+    $("#add1tip").click(function() { setTip(getTip() + 1); });
+    $("#sub1tip").click(function() { setTip(getTip() - 1); });
+
+    $("#pay").val(0);
+    setTip(0);
+
+})()
