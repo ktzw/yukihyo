6117b9bb693d2b99738df165bdbdac6945bb1c52
ktzw <ktzw@users.noreply.github.com>
2014-01-02 09:42:34 +0900

    form

--- javascripts/main.js
+++ javascripts/main.js
@@ -12,11 +12,31 @@
         $("#tip").text(val);
     };
 
-    $("#add3tip").click(function() { setTip(getTip() + 3); });
-    $("#add1tip").click(function() { setTip(getTip() + 1); });
-    $("#sub1tip").click(function() { setTip(getTip() - 1); });
+    var init = function() {
+        $("#pay").val(0);
+        setTip(0);
+    }
 
-    $("#pay").val(0);
-    setTip(0);
+    $("#add3tip").click(function(e){ setTip(getTip() + 3); });
+    $("#add1tip").click(function(e){ setTip(getTip() + 1); });
+    $("#sub1tip").click(function(e){ setTip(getTip() - 1); });
+
+    $("#form").submit(function(e) {
+        e.preventDefault();
+
+        var point = $("#pay").val() | 0;
+        var tip = getTip();
+        $("#result-tbody").append(
+            [point, tip, point + tip * 5].map(function(text){
+                return $("<td/>", { text: text });
+            }).reduce(function(tr, td){
+                return tr.append(td);
+            }, $("<tr/>"))
+        );
+
+        init();
+    });
+
+    init();
 
 })()
